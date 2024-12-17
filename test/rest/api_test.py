36dcf48a1cc273714a3ff2152ca3878694a5436e
japc78 <japc.dev@gmail.com>
2024-12-17 23:43:04 +0100

    feat: :sparkles: CPI1A - Reto 3 | Implement multiply & divide microservices with corresponding integration tests

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -1,7 +1,8 @@
 import http.client
 import os
 import unittest
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 
 import pytest
 
@@ -34,6 +35,36 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/4"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "12", "ERROR MULTIPLY"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/8/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4.0", "ERROR DIVIDE"
+        )
+
+    def test_api_divide_by_zero(self):
+        try:
+            url = f"{BASE_URL}/calc/divide/8/0"
+            req = Request(url, headers={"Accept": "*/*"})
+            response = urlopen(req, timeout=DEFAULT_TIMEOUT)
+            self.fail(f"Se esperaba HTTP 406, pero se obtuvo {response.status}")
+        except HTTPError as e:
+            if e.code == http.client.NOT_ACCEPTABLE:
+                self.assertEqual(e.code, http.client.NOT_ACCEPTABLE, f"Se esperaba HTTP 406, pero se obtuvo {e.code}")
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
